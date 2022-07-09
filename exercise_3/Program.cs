87577d811a30dd7aaacaada506bfd7506cff9bb0
Dara Kosova <darakosova.gb@gmail.com>
2022-07-09 21:02:46 +0400

    Задает массив из 8 эл-в и выводит

---
+++ exercise_3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int[] array = new int[8];
+Random RandomArray = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = RandomArray.Next(0, 8);
+        Console.Write("{0} ", array[i]);
+    }
