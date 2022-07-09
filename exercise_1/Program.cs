5199b12ec4f9c1fafe935ed1810efddde450365f
Dara Kosova <darakosova.gb@gmail.com>
2022-07-09 19:33:15 +0400

    Возвели А в степень В

---
+++ exercise_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.WriteLine ("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine ("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+
+double A_InDegree = Math.Pow(a,b);
+
+Console.WriteLine(A_InDegree);
